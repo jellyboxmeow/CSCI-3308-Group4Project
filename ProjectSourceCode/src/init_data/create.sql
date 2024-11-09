d325fc52f14adc6330ed522faf3c539ffb712158
Alan La <jellyboxmeow@gmail.com>
2024-11-08 21:31:48 -0700

    lab 11 done

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,33 +1,38 @@
 --- TODO: Create a session management table, Users, Login, etc. ---
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE IF NOT EXISTS users(
-    id INT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    users_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password CHAR(60) NOT NULL
 );
 
 ---TODO: Create a table for friends ---
-CREATE TABLE IF NOT EXISTS friends(
-    user_id INT FOREIGN KEY,
-    friend_id INT FOREIGN KEY
+-- CREATE TABLE IF NOT EXISTS friends(
+--     user_id INT FOREIGN KEY,
+--     friend_id INT FOREIGN KEY
+-- );
+
+DROP TABLE IF EXISTS form_comments CASCADE;
+CREATE TABLE IF NOT EXISTS form_comments(
+    form_comments_id INT PRIMARY KEY,
+    -- form_id INT FOREIGN KEY,
+    content TEXT,
+    comment_date DATE
+    -- comment_user FOREIGN KEY
 );
 
 --- TODO: Create table(s) for Community Forms ---
+DROP TABLE IF EXISTS community_forms CASCADE;
 CREATE TABLE IF NOT EXISTS community_forms(
-    id INT PRIMARY KEY,
+    community_forms_id INT PRIMARY KEY,
     form_name VARCHAR(255) NOT NULL,
     form_type VARCHAR(50) NOT NULL,
     form_description TEXT,
-    form_user INT FOREIGN KEY,
+    form_user INT NOT NULL,
+    FOREIGN KEY (form_user) REFERENCES users (users_id) ON DELETE CASCADE,
     form_date DATE,
-    form_comments INT FOREIGN KEY
-);
-
-CREATE TABLE IF NOT EXISTS form-comments(
-    id INT PRIMARY KEY,
-    form_id INT FOREIGN KEY,
-    content TEXT,
-    comment_date DATE,
-    comment_user FOREIGN KEY
+    form_comments_id INT NOT NULL,
+    FOREIGN KEY (form_comments_id) REFERENCES form_comments (form_comments_id) ON DELETE CASCADE
 );
 
 --- TODO: Create a table for Deck value ---
