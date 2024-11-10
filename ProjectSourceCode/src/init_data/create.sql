897ed551dbde4d33fc2933641e269943288a5a1d
Benno Jav <143870197+Virtual-B@users.noreply.github.com>
2024-11-10 09:11:59 -0700

    Benno jav (#29)
    * user status added
    
    * unique usn
    
    * Updated table values and added deck and card tables
    
    ---------
    
    Co-authored-by: Alan La <143768795+jellyboxmeow@users.noreply.github.com>

--- ProjectSourceCode/src/init_data/create.sql
+++ ProjectSourceCode/src/init_data/create.sql
@@ -1,14 +1,16 @@
 --- TODO: Create a session management table, Users, Login, etc. ---
 CREATE TABLE IF NOT EXISTS users(
     id INT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(50) NOT NULL
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password CHAR(60) NOT NULL,
+    status VARCHAR(50) NOT NULL
+    deck_id INT FOREIGN KEY REFERENCES deck(id)
 );
 
 ---TODO: Create a table for friends ---
 CREATE TABLE IF NOT EXISTS friends(
-    user_id INT FOREIGN KEY,
-    friend_id INT FOREIGN KEY
+    user_id INT FOREIGN KEY REFERENCES users(id),
+    friend_id INT FOREIGN KEY REFERENCES users(id),
 );
 
 --- TODO: Create table(s) for Community Forms ---
@@ -31,3 +33,17 @@ CREATE TABLE IF NOT EXISTS form-comments(
 );
 
 --- TODO: Create a table for Deck value ---
+CREATE TABLE IF NOT EXISTS deck(
+    id INT PRIMARY KEY,
+    cards SET FOREIGN KEY REFERENCES cards(id),
+    cards2 SET FOREIGN KEY REFERENCES cards(id)
+);
+
+CREATE TABLE IF NOT EXISTS cards(
+    id INT PRIMARY KEY,
+    card_name VARCHAR(255) NOT NULL,
+    card_image VARCHAR(255) NOT NULL,
+    card_rarity VARCHAR(50) NOT NULL,
+    card_price DECIMAL(10, 2) NOT NULL,
+    card_set VARCHAR(255) NOT NULL
+);
--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,5 +1,5 @@
 --- INSERT test user---
-INSERT INTO users(id, username, password) VALUES (1, 'test', 'test');
+INSERT INTO users(id, username, password) VALUES (1, 'test', 'test', 'admin');
 
 --- INSERT test community form ---
 INSERT INTO community_forms(id, form_name, form_type, form_description, form_user, form_date, form_comments) VALUES (1, 'test form', 'test type', 'test description', 1, '11/3/2024', 1);
