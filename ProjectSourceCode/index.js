8915a20114c71e23f7ec5711e769a1b474e83262
Ben Javier <benjavier1135@gmail.com>
2024-11-14 11:40:10 -0700

    updating my branch

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -1,6 +1,3 @@
-// *****************************************************
-// <!-- Section 1 : Import Dependencies -->
-// *****************************************************
 
 const express = require('express'); // To build an application server or API
 const app = express();
@@ -12,6 +9,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session'); // To set the session object. To store or access session data, use the `req.session`, which is (generally) serialized as JSON by the store.
 const bcrypt = require('bcryptjs'); //  To hash passwords
 const axios = require('axios'); // To make HTTP requests from our server. We'll learn more about it in Part C.
+
 
 // *****************************************************
 // <!-- Section 2 : Connect to DB -->
@@ -59,7 +57,7 @@ app.use(bodyParser.json()); // specify the usage of JSON for parsing request bod
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
-    saveUninitialized: false,
+    saveUninitialized: true,
     resave: false,
   })
 );
@@ -70,32 +68,138 @@ app.use(
   })
 );
 
+
 // *****************************************************
 // <!-- Section 4 : API Routes -->
 // *****************************************************
 
 // TODO - Include your API routes here
 app.get('/', (req, res) => {
-res.redirect('/anotherRoute'); //this will call the /anotherRoute route in the API
+  res.redirect('/anotherRoute'); //this will call the /anotherRoute route in the API
+});
+
+app.get('/welcome', (req, res) => {
+  res.json({ status: 'success', message: 'Welcome!' });
 });
 
 app.get('/anotherRoute', (req, res) => {
-//do something
-res.redirect('/register');
+  //do something
+  res.redirect('/login');
+});
+
+
+app.get('/login', (req, res) => {
+  res.render('pages/login', { error: null })
 });
 
 app.get('/register', (req, res) => {
-    res.render('pages/register', {error:null})
+  res.render('pages/register', { error: null })
+});
+
+app.get('/friends', (req, res) => {
+  res.render('pages/friends', { error: null, friendsList: req.session.friends || []})
+});
+
+// Register
+app.post('/register', async (req, res) => {
+  //hash the password using bcrypt library
+  try {
+    const { username, password } = req.body;
+
+    // Ensure username and password are provided
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Username and password are required' });
+    }
+
+    // Hash the password using bcrypt
+    const hash = await bcrypt.hash(password, 10);
+
+    // Check if the username already exists in the database
+    const userCheckQuery = 'SELECT * FROM users WHERE username = $1';
+    const existingUser = await db.oneOrNone(userCheckQuery, [username]);
+    if (existingUser) {
+      return res.status(400).redirect('/register');
+    }
+
+    // Insert new user into the database
+    const queryText = 'INSERT INTO users (username, password) VALUES ($1, $2)';
+    await db.none(queryText, [username, hash]);
+
+    // Respond with success message
+    // res.status(200).json({ message: 'Success' });
+    console.log('Redirecting to /login');
+    return res.status(302).redirect('/login');
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Registration failed. Please try again.' });
+  }
+});
+
+app.post('/login', async (req, res) => {
+  // Define the SQL query to fetch the user with the provided username
+  const query = 'SELECT * FROM users WHERE username = $1';
+
+  // Query the database for the user. If the user does not exist, 'db.oneOrNone' will return null.
+  const user = await db.oneOrNone(query, [req.body.username]);
+
+  // Compare the hashed password from the database with the one provided in the login form
+  if (user) {
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (match) {
+      req.session.user = user; // Store user data in the session
+      req.session.save(async () => {
+        const friendsListQuery = 'SELECT users.username FROM friends INNER JOIN users \
+        ON users.users_id = friends.friend_id WHERE friends.user_id = $1 \
+        AND users.username != $2\
+        UNION \
+        SELECT users.username FROM friends INNER JOIN users ON \
+        users.users_id = friends.user_id WHERE friends.friend_id = $1 \
+        AND users.username != $2;';
+        // const friendsListQuery = 'SELECT username FROM users WHERE users_id != $1';
+        const friendsList = await db.any(friendsListQuery, [user.users_id, user.username]);
+        req.session.friends = friendsList.map(friend => friend.username); // Extract only the usernames;
+        console.log(friendsList); // checking results
+        console.log(req.session.friends); //debugging session friends
+        req.session.save(()=>{
+          return res.status(302).redirect('/friends'); // Redirect to /friends on successful login
+        });
+      });// Redirect to the 'home' page after successful login
+    } else {
+      return res.status(400).redirect('/login'); // Render the login page with an error message
+    }
+  } else {
+    res.redirect('/register'); // Redirect the user to the registration page
+  }
 });
 
 // Authentication Middleware.
 const auth = (req, res, next) => {
-    if (!req.session.user) {
-        // Default to login page.
-        return res.redirect('/login');
-    }
-    next();
+  if (!req.session.user) {
+    // Default to login page.
+    return res.redirect('/login');
+  }
+  next();
 };
+
+app.get('/search', (req, res) => {
+  const name = req.query.search;
+  console.log(name);
+  axios({
+    url: 'https://api.pokemontcg.io/v2/cards',
+    method: 'GET',
+    params: {
+      q: `name:${name}*`
+    }
+  })
+    .then(results => {
+      console.log(results.data); // the results will be displayed on the terminal if the docker containers are running
+      res.render('pages/search', { cards: results.data.data });
+    })
+    .catch(error => {
+      // Handle errors
+      res.status(400);
+    });
+});
 
 // Authentication Required
 app.use(auth);
@@ -103,5 +207,6 @@ app.use(auth);
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-app.listen(3000);
+module.exports = app.listen(3000);
+// app.listen(3000);
 console.log('Server is listening on port 3000');
--- ProjectSourceCode/src/init_data/insert.sql
+++ ProjectSourceCode/src/init_data/insert.sql
@@ -1,8 +1,26 @@
+-- INSERT test cards --
+INSERT INTO cards(id, card_name, card_image, card_rarity, card_price, card_set) VALUES (1, 'test-charizard', 'cat.png', 'epic', 20.32, 'base');
+INSERT INTO cards(id, card_name, card_image, card_rarity, card_price, card_set) VALUES (2, 'test-squirtle', 'dog.png', 'rare', 0.34, 'base');
+
+-- INSERT test deck --
+INSERT INTO deck(cards, cards2) VALUES (1, 2);
+
 --- INSERT test user---
+<<<<<<< HEAD
 INSERT INTO users(id, username, password) VALUES (1, 'test', 'test', 'admin');
+=======
+INSERT INTO users(username, password, status, deck_id) VALUES ('test', '$2a$10$R4vo/AjcXE5JS0hiJoMPVO7olqCfwMdmM0dWNe7iHPBirtivx/xrC', 'admin', 1);
+INSERT INTO users(username, password) VALUES ('test1', '$2a$10$EgM6nmoF/am09.xLhLb.ieRkHy//UG1lFiH21kARTgoBIMmPBL8Mq');
+>>>>>>> main
 
 --- INSERT test community form ---
-INSERT INTO community_forms(id, form_name, form_type, form_description, form_user, form_date, form_comments) VALUES (1, 'test form', 'test type', 'test description', 1, '11/3/2024', 1);
-INSERT INTO form_comments(id, form_id, content, comment_date, comment_user) VALUES (1, 1, 'test comment', '11/3/2024', 1);
+INSERT INTO form_comments(form_comments_id, content, comment_date) VALUES (1, 'test comment', '11/3/2024');
+INSERT INTO community_forms(community_forms_id, form_name, form_type, form_description, form_user, form_date, form_comments_id) VALUES (1, 'test form', 'test type', 'test description', 1, '11/3/2024', 1);
+-- INSERT INTO form_comments(id, form_id, content, comment_date, comment_user) VALUES (1, 1, 'test comment', '11/3/2024', 1);
+-- INSERT INTO community_forms(id, form_name, form_type, form_description, form_date) VALUES (1, 'test form', 'test type', 'test description', '11/3/2024');
+
+-- INSERT friends --
+INSERT INTO friends(user_id, friend_id) VALUES (1, 2);
+-- INSERT INTO friends(user_id, friend_id) VALUES (1, 3);
 
 --- INSERT test deck values ---
