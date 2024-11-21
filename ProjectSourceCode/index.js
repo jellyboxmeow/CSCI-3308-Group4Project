51bce487ae30dc7f236cd2cf920546b61d096085
Alan La <jellyboxmeow@gmail.com>
2024-11-21 13:02:35 -0700

    database check

--- ProjectSourceCode/index.js
+++ ProjectSourceCode/index.js
@@ -24,7 +24,7 @@ const hbs = handlebars.create({
 
 // database configuration
 const dbConfig = {
-  host: 'db', // the database server
+  host: 'users_db_krc7', // the database server
   port: 5432, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
